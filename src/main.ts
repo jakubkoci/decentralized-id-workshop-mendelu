12e0c32f26f58a2ab2ab5939d9ec38283fab862f
Jakub Koci <jakub.koci@gmail.com>
2024-10-31 22:36:48 +0100

    Add deps and cli command dispatcher

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
 async function main() {
   console.log('Welcome to the Decentralized Digital Identity Workshop!')
+
+  const [command] = process.argv.slice(2)
+
+  switch (command) {
+    case 'list': {
+      // TODO
+      console.log('Not implemented yet')
+      break
+    }
+    case 'present': {
+      // TODO
+      console.log('Not implemented yet')
+      break
+    }
+    case 'receive': {
+      // TODO
+      console.log('Not implemented yet')
+      break
+    }
+    default: {
+      console.log('\nUnknown command\n', command)
+      break
+    }
+  }
 }
 
 main()
